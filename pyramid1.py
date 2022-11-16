f2da889c4b5400cc611c3a03e2b9e74d59bcbcdc
jeet404 <jeet63240@gmail.com>
2022-11-15 20:11:15 -0800

    Add code file.

---
+++ pyramid1.py
@@ -0,0 +1,9 @@
+def drawP(n):
+    for i in range(0, n):
+        for j in range(0, i + 1):
+            print("* ", end="")
+        print("\r")
+
+n = input("Enter Number : ")
+drawP(int(n))
+print("Process Over")
